7a6c7298a2b41ce07cc5b2b54e3ed6d01eb6e4db
kishore <kishorebittu3003@gmail.com>
2022-10-15 12:58:18 +0530

    added

---
+++ inputs.tf
@@ -0,0 +1,10 @@
+variable "target-vpc" {
+    type          = "string"
+    default       = "ap-south-1"
+}
+
+
+variable "cidr_range"{
+    type          = "string"
+    default       ="172.16.0.0/16"
+}
